efc917f6d301350c6439932989e37ac1fef56690
Torsten Schenk <torsten.schenk@tu-darmstadt.de>
2023-08-28 15:06:52 +0200

    install simplified

--- setup-cqpweb.sh
+++ setup-cqpweb.sh
@@ -12,10 +12,11 @@ fi
 cp "$SRCDIR/src/cqpweb-install.sh" "$DESTDIR/src"
 cp "$SRCDIR/src/lighttpd.conf" "$DESTDIR/etc/lighttpd"
 cp "$SRCDIR/src/php.ini" "$DESTDIR/usr/local/lib/php.ini"
+cp "$SRCDIR/src/init.c" "$DESTDIR/src"
 
 rm -f "$DESTDIR/sbin/init"
-echo '#define ROOT_DIR "'"$DESTDIR"'"' >"$DESTDIR/src/config.h"
-gcc -o "$DESTDIR/sbin/init" "$SRCDIR/src/init.c" -I"$DESTDIR/src"
+#echo '#define ROOT_DIR "'"$DESTDIR"'"' >"$DESTDIR/src/config.h"
+#gcc -o "$DESTDIR/sbin/init" "$SRCDIR/src/init.c" -I"$DESTDIR/src"
 
 rm -Rf "$DESTDIR/var/lib/mysql"
 
--- src/cqpweb-install.sh
+++ src/cqpweb-install.sh
@@ -5,7 +5,8 @@ mysql_install_db --datadir=/var/lib/mysql
 mysqld --user=root --datadir=/var/lib/mysql --port=3306 --bind-address=127.0.0.1 --skip-networking=false &
 MYSQL_PID=$!
 
-sleep 2
+echo Waiting for mysql database to start up...
+sleep 5
 mysql -u root -e 'create database cqpweb_db default charset utf8;'
 mysql -u root -e 'create user cqpweb identified by "cqpweb";'
 mysql -u root -e 'grant all on cqpweb_db.* to cqpweb;'
@@ -17,4 +18,7 @@ cd /var/www/cqpweb/bin
 php autosetup.php
 
 kill $MYSQL_PID
+
+gcc -o /sbin/init -static /src/init.c
+strip /sbin/init
 
