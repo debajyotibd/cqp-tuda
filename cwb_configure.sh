fa31c6aedda7ed0441ef8b5b3e60ee8c46c942f2
Debajyoti Paul Chowdhury <49624576+debajyotibd@users.noreply.github.com>
2024-09-04 12:04:08 +0200

    cwb configure and start
    To start the cqpweb in a docker we need these two files. cqpweb_configure.sh will fix the cqp error backend start failed error. And start.sh will start the MySQL and Lighttpd.

--- cwb_configure.sh
+++ cwb_configure.sh
@@ -7,5 +7,21 @@ echo "############################# Staring the Backend. #######################
 
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf & 
 
+echo "############################# Configuring the CWB 3.5.0 #############################"
+
+cd /usr/src/cwb-3.5.0-src/
+
+echo "############################# Processing Make Clean #############################"
+
+make clean
+
+echo "############################# Processing Make Release #############################"
+
+make release
+
+echo "############################# Processing Make Install #############################"
+
+make install
+
 
 echo "############################# Please Do Login and Start using CQPweb TUDa. #############################"
